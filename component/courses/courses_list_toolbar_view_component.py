de5aa731b8f3859ddb0c80d198356bc22b3ff7b4
vshopin <shopin_va@mail.ru>
2025-11-29 17:33:22 +0700

    Практическое применение PageFactory в автотестах.Практикуемся в применении Page Factory

--- component/courses/courses_list_toolbar_view_component.py
+++ component/courses/courses_list_toolbar_view_component.py
@@ -1,23 +1,26 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 import re
 from component.base_component import BaseComponent
+from elements.button import Button
+from elements.text import Text
 
 
 class CoursesListToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
 
-        self.title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id(
-            'courses-list-toolbar-create-course-button'
+        self.title = Text(page, 'courses-list-toolbar-title-text', 'Title')
+        self.create_course_button = Button(
+            page, 'courses-list-toolbar-create-course-button', 'Create course'
         )
 
     def check_visible(self) -> None:
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text('Courses')
+        self.title.check_visible()
+        self.title.check_have_text('Courses')
 
-        expect(self.create_course_button).to_be_visible()
+        self.create_course_button.check_visible()
 
     def click_create_course_button(self) -> None:
         self.create_course_button.click()
         self.check_current_url(re.compile(".*/#/courses/create"))
+
