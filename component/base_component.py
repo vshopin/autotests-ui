3dda427b8315c4ab7f1343dd7a01c7dc9a6ca158
vshopin <shopin_va@mail.ru>
2025-12-06 19:36:23 +0700

    Логирование взаимодействий со страницей.Практика работы с логированием

--- component/base_component.py
+++ component/base_component.py
@@ -1,6 +1,11 @@
+import allure
 from playwright.sync_api import Page, expect
 
 from typing import Pattern
+
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
 
 
 class BaseComponent:
@@ -8,4 +13,8 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]) -> None:
-        expect(self.page).to_have_url(expected_url)
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
