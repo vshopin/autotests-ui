f597c89d78485bb8bacfe921b10acaf4cb29ba14
vshopin <vshopin@users.noreply.github.com>
2025-12-07 09:07:00 +0000

    deploy: d6832eed79132bacafc9ea81a274fa8636f68eb6

--- tools/playwright/mocks.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import Page
-
-
-def mock_static_resourses(page: Page) -> None:
-    page.route("**/*.{ico,png,jpj,svg,webp,mp3,mp4,woff,woff2}", lambda route: route.abort())
--- tools/routes.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class AppRoute(str, Enum):
-    LOGIN = "./#/auth/login"
-    REGISTRATION = "./#/auth/registration"
-    DASHBOARD = "./#/dashboard"
-    COURSES = "./#/courses"
-    CREATE_COURSES = "./#/courses/create"
